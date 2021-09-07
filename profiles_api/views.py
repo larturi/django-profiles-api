b75c8f3461e9a501395089450bd534a99af4265c
lea.arturi <lea.arturi@gmail.com>
2021-09-07 11:31:25 -0300

    Post Hello

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = [
             'Usamos metodos HTTP como funciones (get, post, patch, put, delete)',
@@ -11,3 +17,16 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message', message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
